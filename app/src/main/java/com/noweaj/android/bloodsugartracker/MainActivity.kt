ad82d7ca15806800188c5ad3800d3f937a2978d8
Noweaj.dev <44890643+Noweaj@users.noreply.github.com>
2021-02-16 16:18:30 +0900

    Navigation Component Toolbar Arrow
    Made Navigation component's toolbar arrow to work as navigateUp()

--- app/src/main/java/com/noweaj/android/bloodsugartracker/MainActivity.kt
+++ app/src/main/java/com/noweaj/android/bloodsugartracker/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.noweaj.android.bloodsugartracker
 
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -11,6 +13,8 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private val TAG = MainActivity::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,9 +23,24 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_main_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.fragment_main_chart, R.id.fragment_main_event, R.id.fragment_main_settings))
+//        val appBarConfiguration = AppBarConfiguration(setOf(
+//                R.id.fragment_main_chart, R.id.fragment_main_event, R.id.fragment_main_settings))
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == android.R.id.home) {
+            onBackPressed()
+            return true
+        }
+        return true
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        Log.d(TAG, "onBackPressed")
     }
 }
